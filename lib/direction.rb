3685c5da9ec9419d9600d0b27c437e526901171b
Rocco <roccorighi@hotmail.com>
2017-07-08 17:13:25 +0100

    Fix indentation

--- lib/direction.rb
+++ lib/direction.rb
@@ -1,18 +1,18 @@
 module Direction
-	def N dir
-		dir == 'R' ? 'E' : 'W'
-	end
+  def N dir
+    dir == 'R' ? 'E' : 'W'
+  end
 
-	def S dir
-		dir == 'R' ? 'W' : 'E'
-	end
+  def S dir
+    dir == 'R' ? 'W' : 'E'
+  end
 
-	def W dir
-		dir == 'R' ? 'N' : 'S'
-	end
+  def W dir
+    dir == 'R' ? 'N' : 'S'
+  end
 
-	def E dir
-		dir == 'R' ? 'S' : 'N'
-	end
+  def E dir
+    dir == 'R' ? 'S' : 'N'
+  end
 end
 
--- spec/direction_spec.rb
+++ spec/direction_spec.rb
@@ -2,46 +2,46 @@ require 'spec_helper'
 require 'direction'
 
 describe Direction do
-	let(:extended_class) { Class.new { extend Direction } }
+  let(:extended_class) { Class.new { extend Direction } }
 
-	context 'coming from north' do
-		it 'returns west when turning left' do
-			expect(extended_class.N 'L').to eq 'W'
-		end
+  context 'coming from north' do
+    it 'returns west when turning left' do
+      expect(extended_class.N 'L').to eq 'W'
+    end
 
-		it 'returns east when turning right' do
-			expect(extended_class.N 'R').to eq 'E'
-		end
-	end
+    it 'returns east when turning right' do
+      expect(extended_class.N 'R').to eq 'E'
+    end
+  end
 
-	context 'coming from south' do
-		it 'returns east when turning left' do
-			expect(extended_class.S 'L').to eq 'E'
-		end
+  context 'coming from south' do
+    it 'returns east when turning left' do
+      expect(extended_class.S 'L').to eq 'E'
+    end
 
-		it 'returns west when turning right' do
-			expect(extended_class.S 'R').to eq 'W'
-		end
-	end
+    it 'returns west when turning right' do
+      expect(extended_class.S 'R').to eq 'W'
+    end
+  end
 
-	context 'coming from west' do
-		it 'returns south when turning left' do
-			expect(extended_class.W 'L').to eq 'S'
-		end
+  context 'coming from west' do
+    it 'returns south when turning left' do
+      expect(extended_class.W 'L').to eq 'S'
+    end
 
-		it 'returns north when turning right' do
-			expect(extended_class.W 'R').to eq 'N'
-		end
-	end
+    it 'returns north when turning right' do
+      expect(extended_class.W 'R').to eq 'N'
+    end
+  end
 
-	context 'coming from east' do
-		it 'returns north when turning left' do
-			expect(extended_class.E 'L').to eq 'N'
-		end
+  context 'coming from east' do
+    it 'returns north when turning left' do
+      expect(extended_class.E 'L').to eq 'N'
+    end
 
-		it 'returns south when turning right' do
-			expect(extended_class.E 'R').to eq 'S'
-		end
-	end
+    it 'returns south when turning right' do
+      expect(extended_class.E 'R').to eq 'S'
+    end
+  end
 
 end
