13015467dff600c5893e161f72665bc8faf7cd9b
Rocco <roccorighi@hotmail.com>
2017-07-09 09:10:16 +0100

    Fix spacing

--- lib/mars_rover_parser.rb
+++ lib/mars_rover_parser.rb
@@ -2,9 +2,11 @@ require_relative 'parser'
 
 class MarsRoverParser < Parser
   MOVES = Hash['L','turn_left','R','turn_right', 'M', 'move']
+
   def parse_grid_size line
     line.split().map(&:to_i)
   end
+
   def parse_position line
     *coord, direction = line.split
     [coord.map(&:to_i), direction].flatten
